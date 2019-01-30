e45d8a12ddb9fec0a3b32e00db51e8066e3e9145
Bryan Woods <woods.bryan@gmail.com>
2019-01-30 11:20:17 -0800

    Support Queries of Decodable types
    Previously only scalar types would decode properly here.

--- Decoder/SQLiteQueryDecoder.swift
+++ Decoder/SQLiteQueryDecoder.swift
@@ -10,20 +10,16 @@ struct SQLiteQueryDecoder: UnkeyedDecodingContainer {
 		self.status = sqlite3_errcode(sqlite3_db_handle(stmt))
 	}
 
-	func decoder(for column: Int32) -> SQLiteValueDecoder {
-		return SQLiteValueDecoder(stmt: stmt, column: column)
-	}
-
 // MARK: - UnkeyedDecodingContainer methods
 	var codingPath: [CodingKey] = [ ]
 	var count: Int? = nil // we never know until the query returns SQLITE_DONE
 	var isAtEnd: Bool { return status != SQLITE_ROW }
 	var currentIndex: Int = 0
 
-	func decodeNil() throws -> Bool { return decoder(for: 0).decodeNil() }
+	func decodeNil() throws -> Bool { return SQLiteValueDecoder(stmt: stmt, column: 0).decodeNil() }
 	mutating func decode<T>(_ type: T.Type) throws -> T where T : Decodable {
 		defer { status = sqlite3_step(stmt) }
-		return try decoder(for: 0).decode(T.self)
+		return try T(from: SQLiteDecoder(stmt: stmt))
 	}
 
 	func superDecoder() throws -> Decoder { return SQLiteDecoder(stmt: stmt) }
